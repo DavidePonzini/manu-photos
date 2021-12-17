33a17f7e6a98e2e24a61757088a72c3f4b36bb55
Davide Ponzini <davide.ponzini95@gmail.com>
2021-12-17 19:11:29 +0100

    gps coordinates

--- routes/photos.js
+++ routes/photos.js
@@ -9,27 +9,49 @@ var parser = require('exif-parser');
 
 /* GET photos listing. */
 router.get('/', function(req, res, next) {
-  res.send(JSON.stringify(scan_dir('photos')));
+  let files = scan_dir().map(function(file) {
+    // Handle common data
+    let result = {
+      filename: file.filename,
+      gps: {
+        lat: undefined,
+        lng: undefined
+      },
+      date: new Date(file.metadata.tags.DateTimeOriginal * 1000)
+    };
+
+    // Handle GPS location, if available
+    if(file.metadata.tags.GPSLatitude)
+      result.gps.lat = file.metadata.tags.GPSLatitude;
+    if(file.metadata.tags.GPSLongitude)
+      result.gps.lng = file.metadata.tags.GPSLongitude;
+
+    return result;
+  });
+
+  res.send(JSON.stringify(files));
+});
+
+router.get('/debug', function(req, res, next) {
+  let files = scan_dir();
+  
+  res.send(JSON.stringify(files));
 });
 
 module.exports = router;
 
 
 function scan_dir(dir) {
-  dir = path.join(__dirname, '..', dir);
+  dir = path.join(__dirname, '../public/photos');
   files = fs.readdirSync(dir);
 
   return files.map(function(file) {
     let data = fs.readFileSync(path.join(dir, file));
-    return map_file(file, data);
+    let metadata = parser.create(data).parse();
+
+    return {
+      filename: file,
+      metadata: metadata
+    }
   });
-}
-
-function map_file(file, data) {
-  metadata = parser.create(data).parse();
-
-  return {
-    filename: file,
-    metadata: metadata
-  }
 }
